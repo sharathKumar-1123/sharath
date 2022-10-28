900ddd6697ccce4613785d2c76d08893b1a9f9fb
sharathKumar-1123 <sharathkumarreddy540@gmail.com>
2022-10-28 09:29:50 +0530

    first commit

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Enter number"
+read a 
+echo "Enter another number"
+read b
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
